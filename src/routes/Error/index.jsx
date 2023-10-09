87d47717c32687572611ad04fe1ea481827d7afb
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-10-09 17:24:49 -0300

    error page

--- src/routes/Error/index.jsx
+++ src/routes/Error/index.jsx
@@ -1,10 +1,20 @@
-
+import { Link } from "react-router-dom"
 
 export default function Error(){
 
     return(
-        <main>
-            <h1>Error 404 - Página não encontrada.</h1>
-        </main>
+        <body className='body'>
+            <div className="error">
+                <div className="grid-container">
+                    <div className="grid-error1">
+                    <h1>Error 404 - Página não encontrada.</h1>
+
+                    </div>
+                    <div className="grid-error2">
+                        <button> <Link className="link" to='/'>Voltar</Link></button>
+                    </div>
+                </div>
+            </div>
+        </body>
     )
 }
